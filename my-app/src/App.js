7e10a7427ba17609dc18751c58036d276df3e5c4
Bhanu Mythreyi Ayaluri <bayaluri@gitam.in>
2023-08-09 10:54:02 +0530

    changes made
    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Railway Application
+          React Railway Application(Front end UI)
         </a>
       </header>
     </div>
